ece798bb4bd5d9ecabc3849e56088d28e64f2372
Teemu <Teemu@oma-PC>
2016-02-16 16:40:15 +0200

    Reverted to: "Hello world"

--- main.cpp
+++ main.cpp
@@ -21,9 +21,6 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     cout << "Hello World!\n";
-    cout << "Test commit from 'main' branch\n";
-    cout << "Conflict from 'main'\n";
-    cout << "Conflict test 'SDL_main'\n";
     
     return 0;
 }
